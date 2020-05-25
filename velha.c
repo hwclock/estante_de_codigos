f125fee0ddecacb4930e32b7d4163726ca7fb17b
hwclock <62777145+hwclock@users.noreply.github.com>
2020-05-25 09:52:07 -0300

    Update velha.c

--- velha.c
+++ velha.c
@@ -9,7 +9,7 @@ char *pntP1 = &esp1; char *pntP2 = &esp2; char *pntP3 = &esp3;
 char *pntP4 = &esp4; char *pntP5 = &esp5; char *pntP6 = &esp6;
 char *pntP7 = &esp7; char *pntP8 = &esp8; char *pntP9 = &esp9;
 
-void atualizar_tabuleiro(){
+void atualizar_tabuleiro(){ // função para atualizar tabuleiro
 	system("clear");
 	printf("   %c | %c | %c \n", *pntP1, *pntP2, *pntP3);
 	printf("  -----------\n");
@@ -18,9 +18,14 @@ void atualizar_tabuleiro(){
 	printf("   %c | %c | %c \n",*pntP7, *pntP8, *pntP9);
 }
 
-int verificar_ganhador(){
-		
-		if(*pntP1 == *pntP2 && *pntP2 == *pntP3 && *pntP1 == 'X'){
+int verificar_ganhador(){ // função para verificr ganhador
+		// verifica cada possibilidade de ter um vencedor
+	
+		//return 1 --> jogador 1 ganhou
+		//return 2 --> jogador 2 ganhou
+		//return 0 --> ninguem ganhou ate o momento
+	
+		if(*pntP1 == *pntP2 && *pntP2 == *pntP3 && *pntP1 == 'X'){ 
 			return 1;
 		}
 		else if(*pntP1 == *pntP4 && *pntP4 == *pntP7 && *pntP1 == 'X'){
@@ -76,7 +81,8 @@ int verificar_ganhador(){
 int main(){
 	system("clear");
 	
-	*pntP1 = '1'; *pntP2 = '2'; *pntP3 = '3';
+	//define o numero de cada posição
+	*pntP1 = '1'; *pntP2 = '2'; *pntP3 = '3'; 
 	*pntP4 = '4'; *pntP5 = '5'; *pntP6 = '6';
 	*pntP7 = '7'; *pntP8 = '8'; *pntP9 = '9';
 	
@@ -101,40 +107,40 @@ int main(){
 	
 	system("clear");
 	
-	for(int i = 2; i <= 11; i++){
-		int vezdquem = i % 2;
-		
+	for(int i = 2; i <= 11; i++){ // este laço cuida de quem é a vez de jogar
+		int vezdquem = i % 2; // vai somando a variavel no decorrer do jogo
+				      // se for um numero par, é a vez do jogador 1, se for impar, é a vez do jogador 2
 		ganhador = verificar_ganhador();
-		if(ganhador == 0){
-			if(i == 11){
+		if(ganhador == 0){ // se a função que verifica ganhador retornar 0, ninguem ganhou ainda
+			if(i == 11){ // caso estiver da 9ª jogada, deu velha e para o jogo
 				printf("Deu velha!\n");
 				break;
 			}
 		}
-		else if(ganhador == 1){
+		else if(ganhador == 1){ // o jogador 1 ganhou
 			printf("O ganhador é:\n %s", jogador1);
 			break;
 		}
-		else if(ganhador == 2){
+		else if(ganhador == 2){ // o jogador 2 ganhou
 			printf("O ganhador é:\n %s", jogador2);
 		}
 		
-		if(vezdquem == 0){
+		if(vezdquem == 0){ // é a vez do jogador 1 escolher uma posição
 		    atualizar_tabuleiro();
 		    printf("  Escolha uma posição %s: \n", jogador1);
 		    scanf("  %d", &posicao);
 		    
 		    switch(posicao){
 				
-				case 1:
-					if(*pntP1 == 'O' || *pntP1 == 'X'){
+				case 1: // caso a posição 1 for escolhida
+					if(*pntP1 == 'O' || *pntP1 == 'X'){ // verifica se a posição ja esta ocupada
 						printf("Está posição ja esta ocupada!");
 						i--;
 						break;
 					}
-					*pntP1 = 'X';
+					*pntP1 = 'X'; // coloca o X na posição
 					break;
-				
+				// mesma coisa que o primeiro case
 				case 2:
 					if(*pntP2 == 'O' || *pntP2 == 'X'){
 						printf("Está posição ja esta ocupada!");
@@ -207,14 +213,14 @@ int main(){
 					*pntP9 = 'X';
 					break;	
 					
-				default:
+				default: // caso o valor digitado não for uma posição
 					printf("Digite um opção válida!");
 					i--;
-			}
+			}// fim do switch
 			
 			atualizar_tabuleiro();		
-		}
-		else if(vezdquem == 1){
+		} // fim do if
+		else if(vezdquem == 1){ // é a vez do jogador 2
 		    atualizar_tabuleiro();
 		    printf("Escolha uma posição %s: \n", jogador2);
 		    scanf("%d", &posicao);
@@ -305,10 +311,10 @@ int main(){
 				default:
 					printf("Digite um opção válida!");
 					i--;
-			}
+			} // fim do switch
 			
 			atualizar_tabuleiro();		
-		}
-	}
+		} // fim do else if
+	}// fim do for
 	return 0;
-}
+} // fim do main
